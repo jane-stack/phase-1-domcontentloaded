415018ca7dd7c444cd3c8679198cdb445f6a23fc
Tuyen Ha <tuyen_ha@outlook.com>
2022-07-08 15:23:45 -0600

    commit tree 468fe00e4908ffcc7a1b72532e28cce25a1cecde

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Your code goes here
+// document.addEventListener('DOMContentLoaded', afterLoaded);
+
+// function afterLoaded(e) {
+//     document.getElementById('text').innerHTML = 'This is really cool!';
+// }
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('text').innerHTML = 'This is really cool!';
+})
